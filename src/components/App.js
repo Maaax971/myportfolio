b977a9b1cf626b1c6f9d18bd03883ef714fb4464
Maaax971 <m.dhenin971@gmail.com>
2023-07-12 13:35:47 +0200

    Ajout d'un bouton vers mon Linkedin, ajustement de style, titre dynamique

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,26 @@
-import React from "react"
+import React from "react";
 import Content from "./Content";
 import '../styles/App.css';
-import '../styles/AppResponsive.css'
+import '../styles/AppResponsive.css';
 
 function App() {
+
+  function updateTitle() {
+    var titles = ['Mon Portfolio', 'Maxime Dhénin']; // Tableau contenant les différents titres
+    var index = 0; // Index actuel
+  
+    setInterval(function() {
+      document.title = titles[index]; // Mettre à jour le titre avec l'élément correspondant dans le tableau
+  
+      index++; // Incrémenter l'index
+      if (index === titles.length) {
+        index = 0; // Réinitialiser l'index si on atteint la fin du tableau
+      }
+    }, 3000); // Mettre à jour toutes les 3 secondes
+  };
+
+  updateTitle();
+
   return (
     <div className="App">
 
--- src/components/WhoIAm.js
+++ src/components/WhoIAm.js
@@ -3,9 +3,11 @@ import '../styles/AppResponsive.css';
 
 function WhoIAm(){
     return <div className="mon-parcours">
-        <h1>Compétences et Expériences</h1>
+        <h1>Maxime Dhénin</h1>
+        <a href="https:\\www.linkedin.com/in/maxime-dhenin" target="__blank" className='linkd-btn'>Mon profil Linkedin</a>
         <h2>Mon parcours</h2>
         <p>Diplômé d'un <strong>titre professionnel en développement web (niveau 5)</strong> ainsi que d'un <strong>master en écologie</strong>. Je souhaite approfondir mes connaissances en programmation et apporter mes qualités et ma motivation au sein d'une entreprise.</p>
+        <h2>Compétences et Expériences</h2>
     </div>
 }
 
