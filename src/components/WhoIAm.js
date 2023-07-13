d9c7d5e71c8d95dc017949c4f9f2eec1c63e91b5
Maaax971 <m.dhenin971@gmail.com>
2023-07-13 12:35:16 +0200

    modifications styles mineurs, ajout bouton

--- src/components/WhoIAm.js
+++ src/components/WhoIAm.js
@@ -4,7 +4,10 @@ import '../styles/AppResponsive.css';
 function WhoIAm(){
     return <div className="mon-parcours">
         <h1>Maxime Dhénin</h1>
-        <a href="https:\\www.linkedin.com/in/maxime-dhenin" target="__blank" className='linkd-btn'>Mon profil Linkedin</a>
+        <div className="linkd-btn-div">
+            <a href="https:\\www.linkedin.com/in/maxime-dhenin" target="__blank" className='linkd-btn'>Mon profil Linkedin</a>
+            <a href="https://github.com/Maaax971" target="__blank" className='linkd-btn'>Mon GitHub</a>
+        </div>
         <h2>Mon parcours</h2>
         <p>Diplômé d'un <strong>titre professionnel en développement web (niveau 5)</strong> ainsi que d'un <strong>master en écologie</strong>. Je souhaite approfondir mes connaissances en programmation et apporter mes qualités et ma motivation au sein d'une entreprise.</p>
         <h2>Compétences et Expériences</h2>
